35a33dbafb9873f001a4c108f70a4935c819e349
Roland Müller <rolandman@freenet.de>
2022-01-28 11:32:25 +0100

    strip quotes from link

--- scripts/random-google-image.sh
+++ scripts/random-google-image.sh
@@ -4,5 +4,7 @@ RESULT=$(curl "https://app.zenserp.com/api/v2/search?apikey=916d7170-8019-11ec-8
 LEN=$(echo $RESULT | jq ".image_results | length")
 OFFSET=$(echo $(($RANDOM % $LEN)))
 LINK=$(echo $RESULT | jq ".image_results[$OFFSET].sourceUrl")
-echo "IMAGEURL=$LINK" >> $GITHUB_ENV
+LINK=$(echo $LINK | sed "s/\"//")
+echo "IMAGEURL=$LINK">> $GITHUB_ENV
 echo "image url: $LINK"
+
