5f51aa4f85f82ddfc294dee354b6d973340a4b47
Roland-Georg Müller <roland.mueller@consileo.net>
2022-01-28 12:01:54 +0100

    using file attachment

--- scripts/random-google-image.sh
+++ scripts/random-google-image.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 RESULT=$(curl "https://app.zenserp.com/api/v2/search?apikey=916d7170-8019-11ec-8464-fd5d1d7aefa5&q=Guten%20Morgen&tbm=isch&gl=DE")
 LEN=$(echo $RESULT | jq ".image_results | length")
 OFFSET=$(echo $(($RANDOM % $LEN)))
 LINK=$(echo $RESULT | jq ".image_results[$OFFSET].sourceUrl")
-LINK=$(echo $LINK | sed "s/\"//")
-echo "IMAGEURL=$LINK">> $GITHUB_ENV
-echo "image url: $LINK"
+LINK_STRIPPED=$(echo $LINK | sed "s/\"//g")
+echo "IMAGEURL=$LINK_STRIPPED" >> $GITHUB_ENV
+echo "image url: $LINK_STRIPPED"
+filename=$(basename -- "$LINK_STRIPPED")
+extension="${filename##*.}"
+ext=$(echo $extension | cut -c1-3)
+curl $LINK_STRIPPED --output image.$ext
+echo "IMAGEFILE=image.$ext" >> $GITHUB_ENV
 
