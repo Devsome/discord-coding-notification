991565b4ea2180e480e2f27ffc44fcb50e862ef0
Roland Müller <rolandman@freenet.de>
2022-01-28 10:53:31 +0100

    fix script fetching image url

--- scripts/random-google-image.sh
+++ scripts/random-google-image.sh
@@ -3,8 +3,8 @@
 sudo apt-get install jq
 
 RESULT=$(curl "https://app.zenserp.com/api/v2/search?apikey=916d7170-8019-11ec-8464-fd5d1d7aefa5&q=Guten%20Morgen&tbm=isch&gl=DE")
-LEN=$(echo $RESULT | jq ".image_results.length")
-OFFSET=($RANDOM % $LEN)
+LEN=$(echo $RESULT | jq ".image_results | length")
+OFFSET=$(echo $(($RANDOM % $LEN)))
 LINK=$(echo $RESULT | jq ".image_results[$OFFSET].sourceUrl")
 echo "::set-output name=imageUrl::$LINK"
 
