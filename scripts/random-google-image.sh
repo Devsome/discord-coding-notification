88ecba8a1c77c2a62649d759c5ce0ec576f8e233
Roland Müller <rolandman@freenet.de>
2022-01-28 11:15:46 +0100

    use correct shebang

--- scripts/random-google-image.sh
+++ scripts/random-google-image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 RESULT=$(curl "https://app.zenserp.com/api/v2/search?apikey=916d7170-8019-11ec-8464-fd5d1d7aefa5&q=Guten%20Morgen&tbm=isch&gl=DE")
 LEN=$(echo $RESULT | jq ".image_results | length")
